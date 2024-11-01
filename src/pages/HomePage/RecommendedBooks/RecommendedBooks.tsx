23fbf2af989ae6ba7a0f6e46ccb966f3b67dd62e
Alina Cherevko <alinapavluhina@ukr.net>
2024-11-01 16:32:31 +0200

    add pagination and filtration

--- src/pages/HomePage/RecommendedBooks/RecommendedBooks.tsx
+++ src/pages/HomePage/RecommendedBooks/RecommendedBooks.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
-import { selectRecommendedBooks } from "../../../redux/books/booksSelectors";
+import {
+  selectBooksLoading,
+  selectRecommendedBooks,
+} from "../../../redux/books/booksSelectors";
 import { useSelector } from "react-redux";
 import BooksList from "../BooksList/BooksList";
 import Pagination from "../../../components/Pagination/Pagination";
+import Loader from "../../../components/Loader/Loader";
 
 export type RecBooksProps = {
   setPage: React.Dispatch<React.SetStateAction<number>>;
@@ -13,10 +17,12 @@ export type RecBooksProps = {
 const RecommendedBooks: FC<RecBooksProps> = ({
   setPage,
   page,
-  title,
-  author,
+  // title,
+  // author,
 }) => {
   const { results } = useSelector(selectRecommendedBooks);
+  const isLoading = useSelector(selectBooksLoading);
+
   console.log(results);
   return (
     <div className="bg-gray-bg-color rounded-lg px-5 py-10 tablet:px-10 shrink-0 desktop:w-[847px]">
@@ -26,8 +32,11 @@ const RecommendedBooks: FC<RecBooksProps> = ({
         </h1>
         <Pagination setPage={setPage} page={page} />
       </div>
-      {results && <BooksList results={results} />}
-      {results.length === 0 && (author || title) && (
+      {isLoading ? (
+        <Loader />
+      ) : results && results.length > 0 ? (
+        <BooksList results={results} />
+      ) : (
         <p>Haven't found any book</p>
       )}
     </div>
--- src/redux/books/booksSelectors.ts
+++ src/redux/books/booksSelectors.ts
@@ -2,3 +2,4 @@ import { RootState } from "../store";
 
 export const selectRecommendedBooks = (state: RootState) =>
   state.books.recommended;
+export const selectBooksLoading = (state: RootState) => state.books.isLoading;
