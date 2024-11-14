dd0b6bcbdbc79053f8a31a0ae26a076fe79cb8e1
Alina Cherevko <alinapavluhina@ukr.net>
2024-11-14 10:56:19 +0200

    fix menu

--- src/components/MobMenu/BurgerBtn/BurgerBtn.tsx
+++ src/components/MobMenu/BurgerBtn/BurgerBtn.tsx
@@ -7,7 +7,11 @@ interface BurgerBtnProps {
 
 const BurgerBtn: FC<BurgerBtnProps> = ({ onOpen }) => {
   return (
-    <button className="bg-transparent" type="button" onClick={onOpen}>
+    <button
+      className="bg-transparent tablet:hidden"
+      type="button"
+      onClick={onOpen}
+    >
       <Icon id="icon-menu" stroke="#f9f9f9" width="28px" height="28px" />
     </button>
   );
