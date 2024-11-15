0c53d753c3b7c8ad6d6d78d6d232029086401f29
Alina Cherevko <alinapavluhina@ukr.net>
2024-11-15 16:48:22 +0200

    fix

--- src/components/LangSwitcher/LangSwitcher.tsx
+++ src/components/LangSwitcher/LangSwitcher.tsx
@@ -4,16 +4,18 @@ import { SwitchCheckedChangeDetails } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 
 const LangSwitcher: FC = () => {
-  const [language, setLanguage] = useState("en");
+  const [language, setLanguage] = useState<string | null>(
+    localStorage.getItem("i18nextLng")
+  );
   const { i18n } = useTranslation();
 
   const handleSwitch = (e: SwitchCheckedChangeDetails) => {
-    setLanguage(e.checked ? "en" : "uk");
+    setLanguage(e.checked ? "uk" : "en");
     // i18n.changeLanguage(language);
   };
 
   useEffect(() => {
-    i18n.changeLanguage(language);
+    if (language) i18n.changeLanguage(language);
   }, [language, i18n]);
   return (
     <>
@@ -21,11 +23,11 @@ const LangSwitcher: FC = () => {
         label="111"
         color="gray"
         size="lg"
-        defaultChecked
+        checked={language === "uk"}
         onCheckedChange={handleSwitch}
         trackLabel={{
-          on: <span className="text-primary-white">EN</span>,
-          off: <span className="text-primary-black">UK</span>,
+          on: <span className="text-primary-white">UK</span>,
+          off: <span className="text-primary-black">EN</span>,
         }}
       />
     </>
